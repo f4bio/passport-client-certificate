d7719e46d990e810fce3661e7aed5f1154d2b1d4
Fabio Tea <ft2011@gmail.com>
2015-10-02 05:14:50 +0200

    some updates

--- src/index.js
+++ src/index.js
@@ -1,45 +1,53 @@
-var util = require('util'),
-    Strategy = require('passport-strategy');
+var util = require("util");
+var Strategy = require("passport-strategy");
 
 /*
  * passport.js TLS client certificate strategy
  */
 function ClientCertStrategy(options, verify) {
-  if (typeof options == 'function') {
-    verify = options;
-    options = {};
-  }
-  if (!verify) throw new Error('Client cert authentication strategy requires a verify function');
+	if (typeof options == "function") {
+		verify = options;
+		options = {};
+	}
+	if (!verify) throw new Error("Client cert authentication strategy requires a verify function");
 
-  Strategy.call(this);
-  this.name = 'client-cert';
-  this._verify = verify;
+	Strategy.call(this);
+	this.name = "client-certificate";
+	this._verify = verify;
 }
 
 util.inherits(ClientCertStrategy, Strategy);
 
-ClientCertStrategy.prototype.authenticate = function(req, options) {
-  var that = this;
+ClientCertStrategy.prototype.authenticate = function (req, options) {
+	var that = this;
 
-  // Requests must be authorized
-  // (i.e. the certificate must be signed by at least one trusted CA)
-  if(!req.client.authorized) {
-    that.fail();
-  } else {
-    var clientCert = req.connection.getPeerCertificate();
+	// Requests must be authorized
+	// (i.e. the certificate must be signed by at least one trusted CA)
+	var clientAuth = (req.get("x-ssl-client-verify") == "SUCCESS");
+	if (!clientAuth) {
+		that.fail();
+	} else {
+		var clientCert = req.get("x-ssl-client-cert").toLowerCase();
+		var clientFingerprint = req.get("x-ssl-client-fingerprint").toLowerCase();
+		var clientSerial = req.get("x-ssl-client-serial").toLowerCase();
 
-    // The cert must exist and be non-empty
-    if(!clientCert || Object.getOwnPropertyNames(clientCert).length === 0) {
-      that.fail();
-    } else {
+		// The cert must exist and be non-empty
+		if (!clientCert || Object.getOwnPropertyNames(clientCert).length === 0) {
+			that.fail();
+		} else {
 
-      this._verify(clientCert, function(err, user) {
-        if (err) { return that.error(err); }
-        if (!user) { return that.fail(); }
-        that.success(user);
-      });
-    }
-  }
+			this._verify(clientCert, clientFingerprint, clientSerial,
+				function (err, user) {
+					if (err) {
+						return that.error(err);
+					}
+					if (!user) {
+						return that.fail();
+					}
+					that.success(user);
+				});
+		}
+	}
 };
 
 exports.Strategy = ClientCertStrategy;
